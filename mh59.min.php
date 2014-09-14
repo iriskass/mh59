e187d08fa0c856eeea51ef5b07e988a78dc9e617
Vitalijs Litvinovs <ulow@inbox.lv>
2014-09-14 20:09:45 +0300

    Updated minified version

--- mh59.min.php
+++ mh59.min.php
@@ -1,4 +1,4 @@
 <?
 function mh59($str, $length = 16){
-    $a='';$b=strtolower($b);for($c=0;$c<$d;$c++){$e=hexdec($b[$c]);$f=hexdec($b[(int)(strlen($b)/2)+$c]);$g=$e-$f+($c%8);if($g>0){$a.=chr(65+$g);}else{$a.=chr(122+$g);}}return $a;
+    $a='';$b=strtolower($b);$c=(int)(strlen($b)/2);for($d=0;$d<$e;$d++){$f=hexdec($b[$d]);$g=hexdec($b[$c+$d]);$h=$f-$g+($d%8);if($h>0){$a.=chr(65+$h);}else{$a.=chr(122+$h);}}return $a;
 }
