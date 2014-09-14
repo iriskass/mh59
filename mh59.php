44b9fd49d6f5c302216ba3fcf51ddf865314ef02
Vitalijs Litvinovs <ulow@inbox.lv>
2014-09-14 20:09:04 +0300

    Update mh59.php

--- mh59.php
+++ mh59.php
@@ -2,9 +2,10 @@
 function mh59($str, $length = 16){
     $out = '';
     $str = strtolower($str);
+    $offset = (int)(strlen($str)/2);
     for($i=0; $i<$length; $i++){
         $hex = hexdec($str[$i]);
-        $hex2 = hexdec($str[(int)(strlen($str)/2)+$i]);
+        $hex2 = hexdec($str[$offset+$i]);
 
         $n = $hex - $hex2 + ($i % 8);
 
